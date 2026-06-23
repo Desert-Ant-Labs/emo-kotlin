145f15b572312f7c0177e8c3c22e5bb22fc9d1dc
Finn Voorhees <finnvoorhees@gmail.com>
2026-06-23 16:49:57 +0100

    Release 0.3.0

--- Examples/EmoExample/app/build.gradle.kts
+++ Examples/EmoExample/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-    implementation("com.github.Desert-Ant-Labs:emo-kotlin:0.2.0")
+    implementation("com.github.Desert-Ant-Labs:emo-kotlin:0.3.0")
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.3")
